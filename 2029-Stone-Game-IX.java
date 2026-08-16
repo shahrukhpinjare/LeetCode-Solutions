b9ed9603a720f006fcecd2333139781f07effe47
Mohammad Shahrukh Pinjare <148698182+shahrukhpinjare@users.noreply.github.com>
2026-08-16 21:47:45 +0530

    [2029] [Time Beats: 100.00%] [Memory Beats: 82.43%] - LeetPush

--- 2029-Stone-Game-IX.java
+++ 2029-Stone-Game-IX.java
@@ -2,23 +2,17 @@ class Solution {
     public boolean stoneGameIX(int[] stones) {
         int[] cnt = new int[3];
 
+        // Count stones based on remainder when divided by 3
         for (int stone : stones) {
             cnt[stone % 3]++;
         }
 
-        // Alice needs both types of non-zero remainders.
-        if (cnt[1] == 0 || cnt[2] == 0) {
-            return false;
+        // If count of remainder-0 stones is even
+        if (cnt[0] % 2 == 0) {
+            return cnt[1] > 0 && cnt[2] > 0;
         }
 
-        // If there are no multiples of 3, Alice can win unless
-        // the two groups are too unbalanced.
-        if (cnt[0] == 0) {
-            return Math.abs(cnt[1] - cnt[2]) <= 2;
-        }
-
-        // With multiples of 3, Alice needs a sufficiently large
-        // advantage in one of the two non-zero remainder groups.
-        return cnt[1] > cnt[2] + 2 || cnt[2] > cnt[1] + 2;
+        // If count of remainder-0 stones is odd
+        return Math.abs(cnt[1] - cnt[2]) > 2;
     }
 }
