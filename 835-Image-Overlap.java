5758e8e3ea55a98a550505e7b4f01605b059ed80
Mohammad Shahrukh Pinjare <148698182+shahrukhpinjare@users.noreply.github.com>
2026-09-13 12:34:00 +0530

    [835] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 835-Image-Overlap.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        int maxOverlap = 0;
+
+        // dr = row shift, dc = column shift
+        for (int dr = -(n - 1); dr <= n - 1; dr++) {
+            for (int dc = -(n - 1); dc <= n - 1; dc++) {
+
+                int overlap = 0;
+
+                for (int r = 0; r < n; r++) {
+                    for (int c = 0; c < n; c++) {
+
+                        int nr = r + dr;
+                        int nc = c + dc;
+
+                        // Check if translated position is inside img2
+                        if (nr >= 0 && nr < n && nc >= 0 && nc < n) {
+                            if (img1[r][c] == 1 && img2[nr][nc] == 1) {
+                                overlap++;
+                            }
+                        }
+                    }
+                }
+
+                maxOverlap = Math.max(maxOverlap, overlap);
+            }
+        }
+
+        return maxOverlap;
+    }
+}
